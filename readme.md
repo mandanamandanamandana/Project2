068e7e6268a91fa26ad2a20d4d5c7c7930fc7e48
MandanaMandanaMandana <mandy7373@gmail.com>
2022-12-06 22:19:14 -0500

    initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+## My Project Two Readme
+
+## Description of the App
+
+This app will be a comprehensive list of plant medicines. The home page will be a list of plant medicines with pictures accompanying their names. The user can click on a picture and then will get a description of the plant medicine, different names for it, anecdotes from people who have used them, and the possible medical benefits of each one, as well as dangers each one presents. There will be a back to home button. 
+
+## Link to Planning Trello Board
+
+
+
+![Trello Board](https://trello.com/b/DrqXgVA3/psychedelic-crud)
---
+++ server.js
@@ -0,0 +1,51 @@
+// Dependencies
+
+require("dotenv").config();
+const express = require("express"); //backend framework
+const morgan = require("morgan")//logger (everytime a request comes into the server it will log details of the request)
+const methodOverride = require("method-override")
+const mongoose = require("mongoose")
+const PsychdelicsRouter = require("./controllers/psychdelics")
+
+//Global Variables
+
+//Create Application Object
+
+const app = express(); 
+
+// establish mongo connection
+mongoose.connect(process.env.MONGO)
+
+//Mongoose connection events
+mongoose.connection
+.on("open", () => console.log("Connected to Mongo"))
+.on("close", () => console.log("Disconnected to Mongo"))
+.on("error", (error) => console.log(error))
+
+//Register the middleware
+app.use(morgan("dev"));
+app.use("/static", express.static("public"));
+app.use("/psychedelics", PsychedelicRouter)
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+//---------
+//Routes
+//--------
+
+app.use((request, reponse, next) => {
+    console.log(request.url)
+    next()
+})
+
+app.get("/", (req, res) => {
+    res.send("server is working")
+})
+
+
+//Create server listener
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}`)
+})
